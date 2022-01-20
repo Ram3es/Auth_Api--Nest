9ac5f2f4134ce773e9e9d8a0136580243ac89990
Roman Luzan <romaluzan1@gmail.com>
2022-01-20 18:44:15 +0200

    resolved conflict

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,7 +1,8 @@
-
 import { IsNumber, IsString } from "class-validator"
 
 export class AddRoleDto{
     @IsString({message: "Have to string"})
     readonly value: string
     @IsNumber({}, {message: "Have to number"})
+    readonly userId: number
+}
